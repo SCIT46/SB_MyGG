ff88592a15867a090cab3b6c31809c86c16460e0
user <user@DESKTOP-GCJUTAU>
2024-11-08 13:56:39 +0900

    Springboot initial

---
+++ sb_mygg/src/main/java/com/mysite/sbb/SbMyggApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMyggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMyggApplication.class, args);
+	}
+
+}

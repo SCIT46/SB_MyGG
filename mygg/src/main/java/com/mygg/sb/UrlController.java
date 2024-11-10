3951627c2a72ca4921bd8305a5fddac3095f5d43
sihyeong <syhy7051@gmail.com>
2024-11-10 18:15:17 +0900

    add 리액트 빌드세팅

--- mygg/src/main/java/com/mygg/sb/UrlController.java
+++ mygg/src/main/java/com/mygg/sb/UrlController.java
@@ -1,19 +1,17 @@
 package com.mygg.sb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UrlController {
-	@GetMapping("/main")
-	@ResponseBody
-	public String index(){
-		return "index.html";
-	}
-	
-	@GetMapping("/")	//root 도메인 접속시 리다이렉션
-	public String root() {
-		return "redirect:/main";
-	}
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        // React의 index.html로 모든 비정적 요청을 전달
+        return "forward:/index.html";
+    }
 }
+

e7a5544b5787a118ec7705f1232f70a2f49cd2b3
sorakazae <axsd789@gmail.com>
2024-11-13 19:18:40 +0900

    JSON 분석, 유저정보 등 추출

--- mygg/src/main/java/com/mygg/sb/match/PublicMatch.java
+++ mygg/src/main/java/com/mygg/sb/match/PublicMatch.java
@@ -1,8 +1,10 @@
 package com.mygg.sb.match;
 
+import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -10,57 +12,72 @@ import org.json.simple.parser.JSONParser;
 
 public class PublicMatch {
 
-	final static String RIOT_API_URL = "https://asia.api.riotgames.com";
-	final static String RIOT_API_MATCH = "/lol/match/v5/matches/";
-	final static String API_KEY = "";
+	// final static String RIOT_API_URL = "https://asia.api.riotgames.com";
+	// final static String RIOT_API_MATCH = "/lol/match/v5/matches/";
+	// final static String API_KEY = "RGAPI-bb0375ec-3fc1-4ec0-933a-8800ef3a673e";
 
 
 	public PublicMatch(String matchId) {
 		try{
 			JSONParser parser = new JSONParser();
-			String url = String.format("%s%s%s?api_key=%s",RIOT_API_URL,RIOT_API_MATCH,matchId,API_KEY);
-			System.out.println(url);
-			URL request_url = new URL(url);
-			BufferedReader bf = new BufferedReader(new InputStreamReader(request_url.openStream(),"UTF-8"));
-			String result=bf.readLine();
+
+			//RIOT API JSON
+			// String url = String.format("%s%s%s?api_key=%s",RIOT_API_URL,RIOT_API_MATCH,matchId,API_KEY);
+			// URL request_url = new URL(url);
+			// BufferedReader bf = new BufferedReader(new InputStreamReader(request_url.openStream(),"UTF-8"));
+			// String result=bf.readLine();
+
+			//Local JSON TEST
+			FileReader result = new FileReader("C:\\Spring_git\\sb_MyGG\\mygg\\src\\main\\resources\\testMatch.json");
+
+			ArrayList player = new ArrayList<String>();
+			//JSON Parsing
 
 			JSONObject jsonObject = (JSONObject)parser.parse(result);
 			for(Object key : jsonObject.keySet()){
 				Object value = jsonObject.get(key);
-				if(key instanceof JSONArray){
-					// for(Object obj : value){
-					// }
-				}
-				System.out.println(key+" : "+value);
-			}
-			// JSONObject matchInfo = (JSONObject)jsonObject.get("info");
-			// String infoGameResult = (String)matchInfo.get("endOfGameResult");
-			// Long infoGameCreation = (Long)matchInfo.get("gameCreation");
-			// Long infoGameDuration = (Long)matchInfo.get("gameDuration");
-			// Long infoGameEndtime = (Long)matchInfo.get("gameEndTimestamp");
-			// Long infoGameId = (Long)matchInfo.get("gameId");
-			// String infoGameMode = (String)matchInfo.get("gameMode");
-			// String infoGameName = (String)matchInfo.get("gameName");
-			// Long infoGameStarttime = (Long)matchInfo.get("gameStartTimestamp");
-			// String infoGameType = (String)matchInfo.get("gameType");
-			// String infoGameVersion = (String)matchInfo.get("gameVersion");
-			// Long infoGameMap = (Long)matchInfo.get("mapId");
-			// JSONArray infoParticipants = (JSONArray)matchInfo.get("participants");
 
-			// for(int i=0; i<infoParticipants.size(); i++){
-			// 	JSONObject partOne = (JSONObject)infoParticipants.get(i);
-			// 	Long partAllInPings = (Long)partOne.get("allInPings");
-			// 	Long partAssistMePings = (Long)partOne.get("assistMePings");
-			// 	System.out.println(partAllInPings);
-			// }
-			
+				//Extract Player Identity KEY
+				if(key.equals("metadata")){
+					JSONObject jsonObj = (JSONObject)value;
+					JSONArray participants = (JSONArray)jsonObj.get("participants");
+					for(int i=0; i<participants.size(); i++){
+						player.add(participants.get(i));
+					}
+				}
+				
+				//
+				System.out.println(value.getClass());
+
+				if(key.equals("info")){
+					JSONObject jsonObj = (JSONObject)value;
+					JSONArray participants = (JSONArray)jsonObj.get("participants");
+					for(int i=0; i<participants.size(); i++){
+						JSONObject partPlayer = (JSONObject) participants.get(i);
+
+						
+						String champion = (String) partPlayer.get("championName");
+						String position = (String) partPlayer.get("lane");	//individualPosition, teamPosition
+						String userName = partPlayer.get("riotIdGameName")+"#"+partPlayer.get("riotIdTagline");
+						Long userLevel = (Long) partPlayer.get("summonerLevel");
+						System.out.printf("%d : %s(%d) : %s(%s)",i,userName,userLevel,champion,position);
+
+						JSONObject playerChall = (JSONObject) partPlayer.get("challenges");
+						Double goldPerMin = (Double) playerChall.get("goldPerMinute");
+						System.out.println();
+					}
+				}
+
+			}
+			for(int i=0; i<player.size(); i++){
+				System.out.println("Player"+i+" : "+player.get(i));
+			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		
 	}
 	public static void main(String[] args){
-		System.out.println("start");
 		PublicMatch match = new PublicMatch("KR_7334845449");
 	}
 

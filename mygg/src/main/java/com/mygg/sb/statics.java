69fa53ccb45649faa69c38b48fe998c484be3275
sorakazae <axsd789@gmail.com>
2024-11-20 11:25:21 +0900

    UserApi 1차완성

--- mygg/src/main/java/com/mygg/sb/statics.java
+++ mygg/src/main/java/com/mygg/sb/statics.java
@@ -7,31 +7,40 @@ import java.net.URLEncoder;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.HashMap;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class statics {
-	public static Dotenv dotenv = Dotenv.load();
-	public static final String API_KEY = dotenv.get("RIOT_API_PERSONAL_KEY");
-	public final static String RIOT_API_URL = "https://asia.api.riotgames.com";
-	public final static String RIOT_API_URL_KR = "https://kr.api.riotgames.com";
-	public final static String RIOT_API_MATCH = "/lol/match/v5/matches/";
-	public final static String RIOT_API_SUMMONER_INFO = "/lol/summoner/v4/summoners/by-puuid/";
+    public static Dotenv dotenv = Dotenv.load();
+    public static final String API_KEY = dotenv.get("RIOT_API_PERSONAL_KEY");
+    // =============================================================================================
+    public final static String RIOT_API_URL = "https://asia.api.riotgames.com";
+    public final static String RIOT_API_URL_KR = "https://kr.api.riotgames.com";
+    // =============================================================================================
+    public final static String RIOT_API_ACCOUNT_RID = "/riot/account/v1/accounts/by-riot-id/"; // public match
+    public final static String RIOT_API_ACCOUNT_PID = "/riot/account/v1/accounts/by-puuid/"; // public match
+    public final static String RIOT_API_MATCH = "/lol/match/v5/matches/"; // public match
+    public final static String RIOT_API_MATCH_P = "/lol/rso-match/v1/matches/"; // private match
+    public final static String RIOT_API_SUMMONER_INFO = "/lol/summoner/v4/summoners/by-puuid/";
+    public final static String RIOT_API_LEAGUE = "/lol/league/v4/"; // Iron ~ Diamond
+    public final static String RIOT_API_LEAGUE_EXP = "/lol/league-exp/v4/"; // Master ~ Challenger
+    // =============================================================================================
 
+    // public static String getVersion(){
+    // String versionJSON = new BufferedReader(new InputStreamReader(new
+    // URL("https://ddragon.leagueoflegends.com/api/versions.json").openStream(),"UTF-8")).readLine();
+    // }
+    // public static String getLanguage(){
+    // String languageJSON = new BufferedReader(new InputStreamReader(new
+    // URL("https://ddragon.leagueoflegends.com/cdn/languages.json").openStream(),"UTF-8")).readLine();
+    // }
 
-	// public static String getVersion(){
-        // 	String versionJSON = new BufferedReader(new InputStreamReader(new
-        // 	URL("https://ddragon.leagueoflegends.com/api/versions.json").openStream(),"UTF-8")).readLine();
-	// }
-	// public static String getLanguage(){
-        // 	String languageJSON = new BufferedReader(new InputStreamReader(new
-        // 	URL("https://ddragon.leagueoflegends.com/cdn/languages.json").openStream(),"UTF-8")).readLine();
-	// }
-
-	public static String urlToJson(String url) throws Exception  {
+    public static String urlToJson(String url) throws Exception {
         /* JSON 파일 불러오기/변환 */
 
         // API 키를 요청할 주소값을 URL 타입으로 생성
@@ -46,23 +55,21 @@ public class statics {
 
         // 버퍼 닫기
         bf.close();
-        
-		return userJSON;
-	}
 
-    //gameName(String)과 tagLine(String)를 주면 puuid(String)로 변환
-	public static String nameTagToPid(String gameName, String tagLine) throws Exception {
-        //이름을 URL인코딩하여 처리(영어 외에 처리 안되는 문제 해결)
-        gameName = URLEncoder.encode(gameName,"UTF-8");
+        return userJSON;
+    }
 
-		// API 주소값
-        String user_url = String.format("%s%s%s/%s?api_key=%s", statics.RIOT_API_URL, "/riot/account/v1/accounts/by-riot-id/", gameName, tagLine,
-                statics.API_KEY);
+    // API : gameName(String)과 tagLine(String)를 주면 puuid(String)로 변환
+    public static String nameTagToPid(String gameName, String tagLine) throws Exception {
+        // 이름을 URL인코딩하여 처리(영어 외에 처리 안되는 문제 해결)
+        gameName = URLEncoder.encode(gameName, "UTF-8");
 
-        //url을 json으로 변환
-        String userJSON = statics.urlToJson(user_url);
+        // API 주소값
+        String user_url = String.format("%s%s%s/%s?api_key=%s", RIOT_API_URL, RIOT_API_ACCOUNT_RID, gameName, tagLine,
+                API_KEY);
 
-        // ======================================================================================================================
+        // url을 json으로 변환
+        String userJSON = urlToJson(user_url);
 
         /* JSON Parsing 부 */
 
@@ -73,21 +80,19 @@ public class statics {
         JSONObject jsonObject = (JSONObject) parser.parse(userJSON);
 
         // jsonObject의 JSON Key값으로 모든 데이터 조회
-        String puuid = (String)jsonObject.get("puuid");
-        
-		return puuid;
-	}
+        String puuid = (String) jsonObject.get("puuid");
 
-    //puuid(String)를 주면 gameName, tagLine(String[])으로 변환
+        return puuid; // size : 78
+    }
+
+    // API : puuid(String)를 주면 gameName, tagLine(String[])으로 변환
     public static String[] pidToNametag(String pid) throws Exception {
-		// API 주소값
-        String user_url = String.format("%s%s%s?api_key=%s", statics.RIOT_API_URL, "/riot/account/v1/accounts/by-puuid/", pid,
-                statics.API_KEY);
+        // API 주소값
+        String user_url = String.format("%s%s%s?api_key=%s", RIOT_API_URL, RIOT_API_ACCOUNT_PID, pid,
+                API_KEY);
 
-        //url을 json으로 변환
-        String userJSON = statics.urlToJson(user_url);
-
-        // ======================================================================================================================
+        // url을 json으로 변환
+        String userJSON = urlToJson(user_url);
 
         /* JSON Parsing 부 */
 
@@ -98,18 +103,61 @@ public class statics {
         JSONObject jsonObject = (JSONObject) parser.parse(userJSON);
 
         // jsonObject의 JSON Key값으로 모든 데이터 조회
-        String gameName = (String)jsonObject.get("gameName");
-        String tagLine = (String)jsonObject.get("tagLine");
-        
-		return new String[]{gameName,tagLine} ;
-	}
+        String gameName = (String) jsonObject.get("gameName");
+        String tagLine = (String) jsonObject.get("tagLine");
 
-    //시간을 Epoch(TimeStamp)으로
+        return new String[] {gameName,tagLine};
+    }
+
+    // API : puuid(String)로 summonerId(String) 변환
+    public static String pidToSummonerId(String pid) throws Exception {
+        
+        String user_url = String.format("%s%s%s?api_key=%s", RIOT_API_URL, RIOT_API_SUMMONER_INFO, pid,
+                API_KEY);
+
+        // url을 json으로 변환
+        String userJSON = urlToJson(user_url);
+
+        /* JSON Parsing 부 */
+
+        // JSON 데이터를 분석해주는 JSONParser 객체 생성
+        JSONParser parser = new JSONParser();
+
+        // String 형태의 JSON 데이터를 JSONObject(HashMap)형 jsonObject로 변환
+        JSONObject jsonObject = (JSONObject) parser.parse(userJSON);
+
+        // jsonObject의 JSON Key값으로 모든 데이터 조회
+        String summonerId = (String) jsonObject.get("id");
+
+        return summonerId; // size : 63(max)
+    }
+
+    // API : SummonerId(String)로 소환사 정보(JSONObject) 변환
+    public static JSONObject getLeagueBySummonerId(String summonerId) throws Exception {
+        // puuid로 소환사의 정보를 받아오는 API
+        String user_url = String.format("%s%s%s%s?api_key=%s", RIOT_API_URL_KR, RIOT_API_LEAGUE, "entries/by-summoner/",
+                summonerId, API_KEY);
+
+        // url을 json으로 변환
+        String userJSON = urlToJson(user_url);
+
+        /* JSON Parsing 부 */
+
+        // JSON 데이터를 분석해주는 JSONParser 객체 생성
+        JSONParser parser = new JSONParser();
+
+        // Array안에 담긴 Object 형식으로 오기 때문에 parsing
+        JSONObject jsonObject = (JSONObject)((JSONArray) parser.parse(userJSON)).get(0);
+
+        return jsonObject;
+    }
+
+    // 시간을 Epoch(TimeStamp)으로
     public static long localDateTimeToEpoch(LocalDateTime localDateTime) {
         return localDateTime.toInstant(ZoneOffset.UTC).toEpochMilli();
     }
 
-    //Epoch(TimeStamp)를 시간으로
+    // Epoch(TimeStamp)를 시간으로
     public static LocalDateTime epochToLocalDateTime(long epochMillis) {
         Instant instant = Instant.ofEpochMilli(epochMillis);
         return LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
--- mygg/src/main/java/user/UserApi.java
+++ mygg/src/main/java/user/UserApi.java
@@ -1,7 +1,6 @@
 package user;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -9,6 +8,8 @@ import org.json.simple.parser.JSONParser;
 import com.mygg.sb.statics;
 
 public class UserApi {
+    String puuid;
+    String summonerId;
     //유저 이미지(초상화)
     int profileIconId;
     //유저 이름
@@ -18,18 +19,20 @@ public class UserApi {
     //유저 레벨
     int summonerLevel;
     //유저 등급
-
+    String tier;
+    String rank;
     //유저 lp점수
-
+    int leaguePoints;
     //유저 승패 
-    Boolean win;
+    int wins;
+    int losses;
     //최근 갱신 날짜
     LocalDateTime revisionDate;
 
 
     public UserApi(String gameName, String tagLine) throws Exception{
         // 이름과 태그를 puuid로 변환
-        String puuid = statics.nameTagToPid(gameName,tagLine);
+        puuid = statics.nameTagToPid(gameName,tagLine);
 
         // API 주소값
         String request_url = String.format("%s%s%s?api_key=%s",statics.RIOT_API_URL_KR,statics.RIOT_API_SUMMONER_INFO,puuid,statics.API_KEY);
@@ -47,29 +50,61 @@ public class UserApi {
 		// String 형태의 JSON 데이터를 JSONObject(HashMap)형 jsonObject로 변환
 		JSONObject jsonObject = (JSONObject) parser.parse(summoJSON);
 
-		// jsonObject의 JSON Key값으로 모든 데이터 조회
-        int cnt=0;
-		for (Object key : jsonObject.keySet()) {
-            // key 값으로 받은 데이터를 value에 저장
-            Object value = jsonObject.get(key);
-            switch(cnt){
-                case 1: profileIconId = (int)(long)value;    break;  //profileIconId
-                case 2: System.out.println(value); revisionDate = statics.epochToLocalDateTime((Long)value);  break;
-                case 5: summonerLevel = (int)(long)value;    break; 
-            }
-            //if(key.equals("profileIconId")){}
-            cnt++;
-            //System.out.println(key+" : "+value);
-        }
-        System.out.println(profileIconId);
-        System.out.println(statics.localDateTimeToEpoch(revisionDate));
-        System.out.println(revisionDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SS")));
-        System.out.println(summonerLevel);
+        // jsonObject의 값 추출
+        summonerId = (String) jsonObject.get("id");
+        profileIconId = (int)(long) jsonObject.get("profileIconId");
+        revisionDate = statics.epochToLocalDateTime((long)jsonObject.get("revisionDate"));
+        summonerLevel = (int)(long) jsonObject.get("summonerLevel");
+
+
+		// // jsonObject의 JSON Key값으로 모든 데이터 조회
+        // int cnt=0;
+		// for (Object key : jsonObject.keySet()) {
+        //     // key 값으로 받은 데이터를 value에 저장
+        //     Object value = jsonObject.get(key);
+        //     switch(cnt){
+        //         case 1: profileIconId = (int)(long)value;    break;  //profileIconId
+        //         case 2: System.out.println(value); revisionDate = statics.epochToLocalDateTime((Long)value);  break;
+        //         case 4: summonerId = (String)value;          break;
+        //         case 5: summonerLevel = (int)(long)value;    break; 
+        //     }
+        //     //if(key.equals("profileIconId")){}
+        //     cnt++;
+        //     //System.out.println(key+" : "+value);
+        // }
+
+        // summonerId를 통해 리그정보 JSON 받아옴
+        JSONObject jsonObject2 = statics.getLeagueBySummonerId(summonerId);
+        tier = (String) jsonObject2.get("tier");
+        rank = (String) jsonObject2.get("rank");
+        leaguePoints = (int)(long) jsonObject2.get("leaguePoints");
+        wins = (int)(long) jsonObject2.get("wins");
+        losses = (int)(long) jsonObject2.get("losses");
+
+
+        this.gameName = gameName;
+        this.tagLine = tagLine;
+        // HashMap<String, String> userMap = statics.pidToNametag(puuid);
+        // System.out.println(userMap);
+        // this.gameName = userMap.get("gameName");
+        // this.tagLine = userMap.get("tagLine");
 
 	}
 
 	public static void main(String[] args) throws Exception {
 		UserApi user  = new UserApi("T1 Gumayusi","KR1");
+        System.out.println(user.puuid);
+        System.out.println(user.summonerId);
+        System.out.println(user.profileIconId);
+        System.out.println(user.gameName);
+        System.out.println(user.tagLine);
+        System.out.println(user.summonerLevel);
+        System.out.println(user.tier);
+        System.out.println(user.rank);
+        System.out.println(user.leaguePoints);
+        System.out.println(user.wins);
+        System.out.println(user.losses);
+        System.out.println(user.revisionDate);
 	}
 
 }

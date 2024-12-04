bfe2f8c78817efea7ad18b9629ab391f126ba27f
Junseong <bws4115@naver.com>
2024-12-04 18:54:54 +0900

    runeparsing DTO 만듦

---
+++ mygg/src/main/java/com/mygg/sb/rune/RuneParsingDTO.java
@@ -0,0 +1,16 @@
+package com.mygg.sb.rune;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RuneParsingDTO
+	{
+		private int id;
+		private String key;
+		private String icon;
+		private String name;
+	}

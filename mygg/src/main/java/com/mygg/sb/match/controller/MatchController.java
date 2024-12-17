9088ada207d7f9b0b0d0a39d312bd43e7163979d
Junseong <bws4115@naver.com>
2024-12-17 19:54:55 +0900

    match 전적 뿌려주기 작업중(2)

--- mygg/src/main/java/com/mygg/sb/match/controller/MatchController.java
+++ mygg/src/main/java/com/mygg/sb/match/controller/MatchController.java
@@ -1,7 +1,15 @@
 package com.mygg.sb.match.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mygg.sb.match.MatchDTO;
+import com.mygg.sb.match.PublicMatchService;
 import com.mygg.sb.match.service.MatchService;
 
 import lombok.RequiredArgsConstructor;
@@ -11,6 +19,25 @@ import lombok.RequiredArgsConstructor;
 public class MatchController
 	{
 		private final MatchService service;
-
+		private final PublicMatchService publicService;
+		
+		@GetMapping("/api/match/test/{name}/{tag}")
+		@ResponseBody
+		public List<MatchDTO> getMethodName(@PathVariable(name="name") String name,
+										  @PathVariable(name="tag") String tag) throws Exception 
+		{
+			List<String> matchesIds = service.run(name, tag);
+			List<MatchDTO> getMatchInfo = new ArrayList<>();
+			
+			// matchesIdes[0]: 제일최근, amtchesides[..]: 제일 나중
+			
+			for(int i = 0; i < matchesIds.size(); i++)
+				{
+					getMatchInfo.add(publicService.getMatchInfo(matchesIds.get(i)));
+				}
+			
+			return getMatchInfo;
+		}
+		
 		
 	}

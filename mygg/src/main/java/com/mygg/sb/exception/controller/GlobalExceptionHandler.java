6defbad017324a4c277364ae9efdb03057e4a4cd
Junseong <bws4115@naver.com>
2025-01-14 09:49:21 +0900

    DB, page 기능 테스트중

--- mygg/src/main/java/com/mygg/sb/exception/controller/GlobalExceptionHandler.java
+++ mygg/src/main/java/com/mygg/sb/exception/controller/GlobalExceptionHandler.java
@@ -44,5 +44,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorDTO> handleUnexpectedException(UnexpectedException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorDTO(418,HttpStatus.I_AM_A_TEAPOT, "알 수 없는 오류가 발생했습니다.", e.getMessage()));
     }
+    
+    // 표준 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorDTO(500,HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 오류가 발생했습니다.", e.getMessage()));
+    }
+    
 }
 

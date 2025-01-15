4274ebd6ee2173f1b1406e728e412337d9b67d28
sihyeong <syhy7051@gmail.com>
2025-01-15 11:12:56 +0900

    Merge branch 'master' of https://github.com/SCIT46/SB_MyGG.git into react_est

---
+++ mygg/src/main/java/com/mygg/sb/UrlController.java
@@ -0,0 +1,13 @@
+package com.mygg.sb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UrlController {
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        // React의 index.html로 모든 비정적 요청을 전달
+        return "forward:/index.html";
+    }
+}

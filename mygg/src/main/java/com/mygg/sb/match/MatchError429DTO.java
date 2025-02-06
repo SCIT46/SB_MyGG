31cc99294b806d2dabacc2531de9f6f91008fc80
Junseong <bws4115@naver.com>
2025-02-06 19:05:15 +0900

    큐 시스템 (작성중) Fixes #8

---
+++ mygg/src/main/java/com/mygg/sb/match/MatchError429DTO.java
@@ -0,0 +1,13 @@
+package com.mygg.sb.match;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MatchError429DTO extends MatchDTO
+	{
+		private String waitTime;
+	}

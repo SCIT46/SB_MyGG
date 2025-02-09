9203cddf7f4ce5ceb697723efe102fec6c5794d3
sihyeong <syhy7051@gmail.com>
2025-02-10 08:58:44 +0900

    add analysis components

---
+++ mygg/src/main/front/src/interfaces/IRecentAnaylsis.ts
@@ -0,0 +1,15 @@
+interface IRecentAnaylsis {
+  championId: number;
+  assist: number;
+  championName: string;
+  death: number;
+  gameCnt: number;
+  kda: number;
+  kill: number;
+  loseCnt: number;
+  minionKilled: number;
+  neutralKilled: number;
+  winCnt: number;
+}
+
+export default IRecentAnaylsis;

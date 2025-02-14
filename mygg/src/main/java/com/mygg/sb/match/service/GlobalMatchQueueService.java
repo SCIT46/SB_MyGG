f8bec45f52868e7cebc672f7f8805ffece4140bf
Junseong <bws4115@naver.com>
2025-02-14 19:50:12 +0900

    큐 시스템 비동기로 구현 중

--- mygg/src/main/java/com/mygg/sb/match/service/GlobalMatchQueueService.java
+++ mygg/src/main/java/com/mygg/sb/match/service/GlobalMatchQueueService.java
@@ -1,43 +1,59 @@
 package com.mygg.sb.match.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.mygg.sb.exception.custom.RiotApiTooManyRequests;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.*;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class GlobalMatchQueueService 
 {
-    private final ConcurrentLinkedQueue<String> globalQueue;
-    private final PublicMatchService publicMatchService;
+    private final ConcurrentLinkedQueue<String> globalQueue = new ConcurrentLinkedQueue<>();
+    private PublicMatchService publicMatchService;
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();  // 하나의 쓰레드만 사용
+    private boolean isQueueProcessing = false;  // 큐 처리가 진행 중인지 여부
 
+    @Lazy // 지연 로딩 적용
+    @Autowired
+    public void setPublicMatchService(PublicMatchService publicMatchService) 
+    {
+        this.publicMatchService = publicMatchService;
+        // ExecutorService를 생성하여 하나의 쓰레드에서만 큐를 처리하도록 함
+    }
+    
     // 앞선 동작에 끝나고 0.8초 뒤에 한 번씩 작동 (1000 = 1초)
-    @Scheduled(fixedDelay = 800) 
+    @Scheduled(fixedDelay = 10000) 
     public void run() throws Exception
     {
+    	if(globalQueue.isEmpty()) return;
+    	
         try 
         {
-            publicMatchService.changeJSONToDTOMatchData(globalQueue.poll());
-            log.info("=========== queue 시스템 Test ==========");
-            log.info("동작완료");
+        	System.out.println("======================= run 작동 =======================");
+        	System.out.println("globalQueue size: " + globalQueue.size());
+        	
+        	publicMatchService.changeJSONToDTOMatchData(globalQueue.peek());
+        	globalQueue.poll();
         } 
         catch (RiotApiTooManyRequests e) 
         {
-            log.error("queueSystem: 리퀘스트 요청초과" + e.getRetryAfter() + "대기합니다", e);
-            log.error("queueSystem: getXAppRateLimit" + e.getXAppRateLimit());
-            log.error("queueSystem: getXAppRateLimitCount" + e.getXAppRateLimitCount());
+        	System.out.println("======================= sleep 작동 =======================");
+        	System.out.println(Integer.parseInt(e.getRetryAfter()));
+        	
             Thread.sleep(Integer.parseInt(e.getRetryAfter()));
         }
         catch (Exception e)
         {
+        	System.out.println("======================= exception Err=======================");
+        	System.out.println(e.getMessage());
             throw new Exception("Queue System에서 에러가 발생했습니다" + e.getMessage());
         }
     }
@@ -45,6 +61,11 @@ public class GlobalMatchQueueService
     // 매치 ID를 큐에 추가
     public void addAll(List<String> matchIds) 
     {
+    	System.out.println("================== addAll 작동 ==================");
+    	System.out.println(matchIds);
+    	
+    	matchIds.forEach(name->System.out.println(name));
+    	
         globalQueue.addAll(matchIds);
     }
 

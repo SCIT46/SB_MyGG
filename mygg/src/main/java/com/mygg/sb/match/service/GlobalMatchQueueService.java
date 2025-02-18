7ca42b6fb7b876afc67f19feeb92c9db55c0d71e
Junseong <bws4115@naver.com>
2025-02-18 18:18:49 +0900

    commit

--- mygg/src/main/java/com/mygg/sb/match/service/GlobalMatchQueueService.java
+++ mygg/src/main/java/com/mygg/sb/match/service/GlobalMatchQueueService.java
@@ -1,46 +1,13 @@
 package com.mygg.sb.match.service;
-<<<<<<< HEAD
-=======
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
->>>>>>> queueSystem
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.mygg.sb.exception.custom.RiotApiTooManyRequests;
 
-<<<<<<< HEAD
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class GlobalMatchQueueService 
-{
-    private final ConcurrentLinkedQueue<String> globalQueue;
-    private final PublicMatchService publicMatchService;
-
-    // 앞선 동작에 끝나고 0.8초 뒤에 한 번씩 작동 (1000 = 1초)
-    @Scheduled(fixedDelay = 800) 
-    public void run() throws Exception
-    {
-        try 
-        {
-            publicMatchService.changeJSONToDTOMatchData(globalQueue.poll());
-            log.info("=========== queue 시스템 Test ==========");
-            log.info("동작완료");
-        } 
-        catch (RiotApiTooManyRequests e) 
-        {
-            log.error("queueSystem: 리퀘스트 요청초과" + e.getRetryAfter() + "대기합니다", e);
-            log.error("queueSystem: getXAppRateLimit" + e.getXAppRateLimit());
-            log.error("queueSystem: getXAppRateLimitCount" + e.getXAppRateLimitCount());
-=======
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.*;
@@ -80,16 +47,12 @@ public class GlobalMatchQueueService
         	// System.out.println("======================= sleep 작동 =======================");
         	// System.out.println(Integer.parseInt(e.getRetryAfter()));
         	
->>>>>>> queueSystem
             Thread.sleep(Integer.parseInt(e.getRetryAfter()));
         }
         catch (Exception e)
         {
-<<<<<<< HEAD
-=======
         	// System.out.println("======================= exception Err=======================");
         	// System.out.println(e.getMessage());
->>>>>>> queueSystem
             throw new Exception("Queue System에서 에러가 발생했습니다" + e.getMessage());
         }
     }
@@ -97,14 +60,11 @@ public class GlobalMatchQueueService
     // 매치 ID를 큐에 추가
     public void addAll(List<String> matchIds) 
     {
-<<<<<<< HEAD
-=======
     	// System.out.println("================== addAll 작동 ==================");
     	// System.out.println(matchIds);
     	
     	// matchIds.forEach(name->System.out.println(name));
     	
->>>>>>> queueSystem
         globalQueue.addAll(matchIds);
     }
 
